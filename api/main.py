17969b2661a505bcb47a066671f628754e50e4be
박성호 <parksh5573@gmail.com>
2026-08-25 16:47:26 +0900

    allow localhost:5173 origin for CORS

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Spotify Mini Project API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ["FRONTEND_URL"]],
+    allow_origins=[os.environ["FRONTEND_URL"], "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
